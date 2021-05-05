b6cf6c9bee93472f2cfe0641ddaaa8060781f64c
Andres Gamba <xnovaerax@gmail.com>
2021-05-05 07:51:49 -0500

    Cambio a ws

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# Node.js Socket.io Example
+# Node.js WebSocket Example
 
 This example accompanies the
 [Using WebSockets on Heroku with Node.js](https://devcenter.heroku.com/articles/node-websockets)
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-const socketIO = require('socket.io');
+const { Server } = require('ws');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
@@ -10,11 +10,15 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const io = socketIO(server);
+const wss = new Server({ server });
 
-io.on('connection', (socket) => {
+wss.on('connection', (ws) => {
   console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+  ws.on('close', () => console.log('Client disconnected'));
 });
 
-// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+/* setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000); */
