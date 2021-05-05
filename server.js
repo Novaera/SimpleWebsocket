ea7c7288f1358202b1b2f19c177dd463486e92f8
Andres Gamba <xnovaerax@gmail.com>
2021-05-05 08:17:15 -0500

    Fixes

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 'use strict';
 
 const express = require('express');
-const { Server } = require('ws');
+const { Server: WebSocket } = require('ws');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const wss = new Server({ server });
+const wss = new WebSocket({ server });
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
+    ws.on('message', function incoming(data) {
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === 1) {
+                console.log(data);
+                client.send(data);
+            }
+        })
     })
-  })
 });
