e179a4898a35ad1528291ae3a5819ffca970d29e
Andres Gamba <xnovaerax@gmail.com>
2021-05-05 07:20:56 -0500

    Update

--- server.js
+++ server.js
@@ -17,4 +17,4 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
